ae0f689618fcdd6cc44ca0c525f43be8a27d7b84
rkrux <5960750+rkrux@users.noreply.github.com>
2022-09-29 19:43:17 +0530

    Temp UI display

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import paymentsQueries from './paymentData/index';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <>
+        {paymentsQueries.map((result) => (
+          <p>
+            <div>{`${result.dateRange.startDate} -> ${result.dateRange.endDate}`}</div>
+            <div>{result.zeroConfTranxPercent.toFixed(2)} %</div>
+            <div>{result.zeroConfTranxAvgSpeed.toFixed(2)} mins</div>
+            <div>{result.onchainConfTranxAvgSpeed.toFixed(2)} hrs</div>
+          </p>
+        ))}
+      </>
+    </div>
+  );
 }
 
 export default App;
