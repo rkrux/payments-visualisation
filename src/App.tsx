e0888b673f182bab3b09bb32af4955a74d669d0c
rkrux <5960750+rkrux@users.noreply.github.com>
2022-09-30 02:08:58 +0530

    Added date range picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import './App.css';
 import { fetchPaymentsData } from './paymentsData/index.ts';
 import { useQuery } from 'react-query';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 
 function usePaymentsQuery(dateRange) {
   return useQuery(['payments', dateRange], async () => {
@@ -9,11 +11,28 @@ function usePaymentsQuery(dateRange) {
   });
 }
 
+function DateRange({ dateRange, updateDateRange }) {
+  const [value, onChange] = useState([dateRange.startDate, dateRange.endDate]);
+
+  return (
+    <DateRangePicker
+      value={value}
+      onChange={(newDates) => {
+        onChange(newDates);
+        updateDateRange({ startDate: newDates[0], endDate: newDates[1] });
+      }}
+      minDate={new Date('2021-01-01')}
+      maxDate={new Date('2021-05-31')}
+    />
+  );
+}
+
 function App() {
-  const { isLoading, isError, error, data } = usePaymentsQuery({
-    startDate: new Date('2021-01-01'),
-    endDate: new Date('2021-05-31'),
+  const [dateRange, setDateRange] = useState({
+    startDate: new Date('2021-03-15'),
+    endDate: new Date('2021-03-16'),
   });
+  const { isLoading, isError, error, data } = usePaymentsQuery(dateRange);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -25,6 +44,10 @@ function App() {
 
   return (
     <div>
+      <DateRange
+        dateRange={dateRange}
+        updateDateRange={(dr) => setDateRange(dr)}
+      />
       <p>{JSON.stringify(data.tranxPercentsAndTimes)}</p>
       <p>{JSON.stringify(data.userWalletsInDateRangeByGranularity)}</p>
       <p>{JSON.stringify(data.userWalletsBreakdownInDateRange)}</p>
