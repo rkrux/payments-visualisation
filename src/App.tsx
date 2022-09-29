be610fa5b01369fe7772626972ef08550640b28a
rkrux <5960750+rkrux@users.noreply.github.com>
2022-09-30 02:52:12 +0530

    Added recharts and basic vizs

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import './App.css';
 import { fetchPaymentsData } from './paymentsData/index.ts';
 import { useQuery } from 'react-query';
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  Legend,
+  LineChart,
+  CartesianGrid,
+  Line,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 function usePaymentsQuery(dateRange) {
   return useQuery(['payments', dateRange], async () => {
@@ -24,6 +35,46 @@ function DateRange({ dateRange, updateDateRange }) {
       minDate={new Date('2021-01-01')}
       maxDate={new Date('2021-05-31')}
     />
+  );
+}
+
+function BreakdownViz({ data }) {
+  return (
+    <PieChart width={730} height={250}>
+      <Pie
+        data={data}
+        nameKey="metricKey"
+        dataKey="metricValue"
+        cx="50%"
+        cy="50%"
+        outerRadius={50}
+        fill="#8884d8"
+      />
+      <Tooltip />
+      <Legend />
+    </PieChart>
+  );
+}
+
+function TrendViz({ data }) {
+  const metricLines = Object.keys(data[0])
+    .filter((key) => key !== 'timePeriod')
+    .map((key) => <Line type="monotone" dataKey={key} />);
+
+  return (
+    <LineChart
+      width={730}
+      height={250}
+      data={data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="timePeriod" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      {metricLines}
+    </LineChart>
   );
 }
 
@@ -49,10 +100,10 @@ function App() {
         updateDateRange={(dr) => setDateRange(dr)}
       />
       <p>{JSON.stringify(data.tranxPercentsAndTimes)}</p>
-      <p>{JSON.stringify(data.userWalletsInDateRangeByGranularity)}</p>
-      <p>{JSON.stringify(data.userWalletsBreakdownInDateRange)}</p>
-      <p>{JSON.stringify(data.paymentMethodsInDateRangeByGranularity)}</p>
-      <p>{JSON.stringify(data.paymentMethodsBreakdownInDateRange)}</p>
+      <TrendViz data={data.userWalletsInDateRangeByGranularity} />
+      <BreakdownViz data={data.userWalletsBreakdownInDateRange} />
+      <TrendViz data={data.paymentMethodsInDateRangeByGranularity} />
+      <BreakdownViz data={data.paymentMethodsBreakdownInDateRange} />
     </div>
   );
 }
