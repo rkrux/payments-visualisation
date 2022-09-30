3c7952825225cc90490c046894ac20c356b3b3ce
rkrux <5960750+rkrux@users.noreply.github.com>
2022-09-30 16:35:34 +0530

    Added basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -26,60 +26,75 @@ function DateRange({ dateRange, updateDateRange }) {
   const [value, onChange] = useState([dateRange.startDate, dateRange.endDate]);
 
   return (
-    <DateRangePicker
-      value={value}
-      onChange={(newDates) => {
-        onChange(newDates);
-        updateDateRange({ startDate: newDates[0], endDate: newDates[1] });
-      }}
-      minDate={new Date('2021-01-01')}
-      maxDate={new Date('2021-05-31')}
-    />
-  );
-}
-
-function BreakdownViz({ data }) {
-  return (
-    <PieChart width={730} height={250}>
-      <Pie
-        data={data}
-        nameKey="metricKey"
-        dataKey="metricValue"
-        cx="50%"
-        cy="50%"
-        outerRadius={50}
-        fill="#8884d8"
+    <div id="dateRange" className="paddedCenter">
+      <DateRangePicker
+        value={value}
+        onChange={(newDates) => {
+          onChange(newDates);
+          updateDateRange({ startDate: newDates[0], endDate: newDates[1] });
+        }}
+        minDate={new Date('2021-01-01')}
+        maxDate={new Date('2021-05-31')}
       />
-      <Tooltip />
-      <Legend />
-    </PieChart>
+    </div>
   );
 }
 
-function TrendViz({ data }) {
+function BreakdownViz({ data, id, title }) {
+  return (
+    <div id={id} className="paddedCenter">
+      <div>
+        <h3 className="center">{title}</h3>
+        <PieChart width={730} height={250}>
+          <Pie
+            data={data}
+            nameKey="metricKey"
+            dataKey="metricValue"
+            outerRadius="100%"
+            fill="#8884d8"
+          />
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </div>
+    </div>
+  );
+}
+
+function TrendViz({ data, id, title }) {
   const metricLines = Object.keys(data[0])
     .filter((key) => key !== 'timePeriod')
     .map((key) => <Line type="monotone" dataKey={key} />);
 
   return (
-    <LineChart
-      width={730}
-      height={250}
-      data={data}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="timePeriod" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      {metricLines}
-    </LineChart>
+    <div id={id} className="paddedCenter">
+      <div>
+        <h3 className="center">{title}</h3>
+        <LineChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timePeriod" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {metricLines}
+        </LineChart>
+      </div>
+    </div>
   );
 }
 
 function Headline({ metricKey, metricValue }) {
-  return <div className="headline">{`${metricKey}: ${metricValue}`}</div>;
+  return (
+    <div className="headline">
+      <div className="title">{metricKey}</div>
+      <div className="value">{metricValue}</div>
+    </div>
+  );
 }
 
 function Headlines({ data }) {
@@ -94,11 +109,11 @@ function Headlines({ data }) {
         metricValue={`${data.zeroConfTranxPercent.toFixed(2)}%`}
       />
       <Headline
-        metricKey="0-Conf Transactions Avg Time"
+        metricKey="0-Conf Avg Time"
         metricValue={`${data.zeroConfTranxAvgSpeed.toFixed(2)} mins`}
       />
       <Headline
-        metricKey="On-chain Transactions Avg Time"
+        metricKey="On-chain Avg Time"
         metricValue={`${data.onchainConfTranxAvgSpeed.toFixed(2)} hrs`}
       />
     </div>
@@ -107,8 +122,8 @@ function Headlines({ data }) {
 
 function App() {
   const [dateRange, setDateRange] = useState({
-    startDate: new Date('2021-03-15'),
-    endDate: new Date('2021-03-16'),
+    startDate: new Date('2021-03-02'),
+    endDate: new Date('2021-03-28'),
   });
   const { isLoading, isError, error, data } = usePaymentsQuery(dateRange);
 
@@ -121,16 +136,34 @@ function App() {
   }
 
   return (
-    <div>
-      <DateRange
-        dateRange={dateRange}
-        updateDateRange={(dr) => setDateRange(dr)}
-      />
-      <Headlines data={data.tranxPercentsAndTimes} />
-      <TrendViz data={data.userWalletsInDateRangeByGranularity} />
-      <BreakdownViz data={data.userWalletsBreakdownInDateRange} />
-      <TrendViz data={data.paymentMethodsInDateRangeByGranularity} />
-      <BreakdownViz data={data.paymentMethodsBreakdownInDateRange} />
+    <div className="app">
+      <div className="container">
+        <DateRange
+          dateRange={dateRange}
+          updateDateRange={(dr) => setDateRange(dr)}
+        />
+        <Headlines data={data.tranxPercentsAndTimes} />
+        <BreakdownViz
+          id="userWalletsBreakdown"
+          title="User Wallets Breakdown"
+          data={data.userWalletsBreakdownInDateRange}
+        />
+        <TrendViz
+          id="userWalletsTrend"
+          title="User Wallets Trend"
+          data={data.userWalletsInDateRangeByGranularity}
+        />
+        <BreakdownViz
+          id="paymentMethodsBreakdown"
+          title="Payment Methods Breakdown"
+          data={data.paymentMethodsBreakdownInDateRange}
+        />
+        <TrendViz
+          id="paymentMethodsTrend"
+          title="Payment Methods Trend"
+          data={data.paymentMethodsInDateRangeByGranularity}
+        />
+      </div>
     </div>
   );
 }
