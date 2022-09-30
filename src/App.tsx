9932bbcea6b893545f135b757e5f454e43111fb8
rkrux <5960750+rkrux@users.noreply.github.com>
2022-09-30 23:32:31 +0530

    Separated date range comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
 import { useState } from 'react';
-import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 import { usePaymentsQuery } from './paymentsData/index.ts';
 import { Headlines, BreakdownViz, TrendViz } from './visualisations/index.tsx';
+import DateRange from './dateRange';
 import { DEFAULT_DATE_RANGE } from './constants.ts';
 import './App.css';
-
-function DateRange({ dateRange, updateDateRange }) {
-  const [value, onChange] = useState([dateRange.startDate, dateRange.endDate]);
-
-  return (
-    <div id="dateRange" className="paddedCenter">
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <span className="prompt">Select date range:</span>
-        <DateRangePicker
-          value={value}
-          onChange={(newDates) => {
-            onChange(newDates);
-            updateDateRange({ startDate: newDates[0], endDate: newDates[1] });
-          }}
-          minDate={new Date('2021-01-01')}
-          maxDate={new Date('2021-05-31')}
-          clearIcon={null}
-        />
-      </div>
-    </div>
-  );
-}
 
 function App() {
   const [dateRange, setDateRange] = useState(DEFAULT_DATE_RANGE);
---
+++ src/dateRange/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { DEFAULT_DATE_RANGE } from '../constants';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+
+export default function DateRange({ dateRange, updateDateRange }) {
+  const [value, onChange] = useState([dateRange.startDate, dateRange.endDate]);
+
+  return (
+    <div id="dateRange" className="paddedCenter">
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <span className="prompt">Select date range:</span>
+        <DateRangePicker
+          value={value}
+          onChange={(newDates) => {
+            onChange(newDates);
+            updateDateRange({ startDate: newDates[0], endDate: newDates[1] });
+          }}
+          minDate={DEFAULT_DATE_RANGE.startDate}
+          maxDate={DEFAULT_DATE_RANGE.endDate}
+          clearIcon={null}
+        />
+      </div>
+    </div>
+  );
+}
